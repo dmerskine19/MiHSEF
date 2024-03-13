4b76ea16c5db5c39612f1934269dcbfcd9c6bf89
dmerskine19 <Dmerskine19@gmail.com>
2024-03-12 23:57:28 -0400

    3-13

--- business_logic/services/AWS_SCS.py
+++ business_logic/services/AWS_SCS.py
@@ -7,12 +7,11 @@ def connect_to_s3(access_key_id, secret_access_key):
     s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
     return s3
 
+
 def download_file(s3, bucket_name, file_key, destination_path):
     # Logic to download file from AWS S3 bucket
     s3.download_file(bucket_name, file_key, destination_path)
 
-
-import boto3
 
 def send_email():
     # Create an SES client
